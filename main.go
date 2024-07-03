d5d46bff5ec9f172bdc3106319aaed05cd5a1160
FilippoTrotter <129587442+FilippoTrotter@users.noreply.github.com>
2024-07-03 09:31:11 +0200

    Refactor cobra (#32)
    Use cobra toolkit and consequent  refactor of all the code
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a command-line tool `tgcom` that allows
    commenting/uncommenting code in various programming languages.
    - Added a terminal user interface (TUI) for interactive file selection
    and mode selection using Bubble Tea.
    - Implemented support for flag-based interactions for file
    manipulations.
    
    - **Bug Fixes**
      - Corrected package naming in tests to ensure consistency.
    
    - **Tests**
    - Added comprehensive tests for file modifications, comment handling,
    backups, and user input validation in various modules.
    
    - **Refactor**
    - Streamlined utility functions for directory navigation and file path
    management.
    
    - **Documentation**
    - Enhanced documentation for new functionalities and usage instructions
    for the `tgcom` tool.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Filippo Trotter <filippotrotter@MBPdiFilippo2.fritz.box>
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>
    Co-authored-by: ManudL2000 <e.dilernia5@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/dyne/tgcom/cmd"
+	"github.com/dyne/tgcom/utils/tui"
+	"github.com/dyne/tgcom/utils/tui/modelutils"
+)
+
+func main() {
+	//function main must be modified, but it shows that it works both with
+	//flags and as a tui
+	var mod int = 1
+	var num int
+	if mod == 1 {
+		cmd.Execute()
+		_, err := fmt.Scanf("%d", &num)
+		if err != nil {
+			fmt.Println("Errore:", err)
+		}
+	}
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting current working directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Initialize your model with the current directory
+	model := tui.Model{
+		State:         "FileSelection",
+		FilesSelector: modelutils.InitialModel(currentDir, 20),
+	}
+
+	// Bubble Tea program
+	p := tea.NewProgram(model)
+
+	// Start the program
+	if _, err := p.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting Bubble Tea program: %v\n", err)
+		os.Exit(1)
+	}
+}
