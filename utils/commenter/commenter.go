fe7167e39021d995232477c004fd2bd03528b61c
Filippo Trotter <filippotrotter@MBPdiFilippo2.fritz.box>
2024-07-09 12:43:51 +0200

    refactor

--- utils/commenter/commenter.go
+++ utils/commenter/commenter.go
@@ -16,22 +16,13 @@ func Uncomment(line string, char string) string {
 	trimmedLine := strings.TrimSpace(line)
 
 	//just for html
-	if char == "<!-- -->" {
-		if strings.HasPrefix(trimmedLine, "<!--") && strings.HasSuffix(trimmedLine, "-->") {
-			// Check for both `<!--` and `<!-- ` prefixes.
-			if strings.HasPrefix(trimmedLine, "<!--"+" ") {
-				line = strings.Replace(line, "<!-- ", "", 1)
-				line = strings.Replace(line, "<!--", "", 1)
-			}
+	if char == "<!-- -->" && strings.HasPrefix(trimmedLine, "<!--") && strings.HasSuffix(trimmedLine, "-->") {
+		line = strings.Replace(line, "<!-- ", "", 1)
+		line = strings.Replace(line, "<!--", "", 1)
+		line = strings.Replace(line, " "+"-->", "", 1)
+		line = strings.Replace(line, "-->", "", 1)
 
-			// Check for both '-->' and ' -->' suffixes
-			if strings.HasSuffix(trimmedLine, " "+"-->") {
-				line = strings.Replace(line, " "+"-->", "", 1)
-				line = strings.Replace(line, "-->", "", 1)
-			}
-
-			return line
-		}
+		return line
 	}
 
 	if strings.HasPrefix(trimmedLine, char) {
@@ -49,16 +40,14 @@ func ToggleComments(line string, char string) string {
 	trimmedLine := strings.TrimSpace(line)
 
 	//just for html
-	if char == "<!-- -->" {
-		if strings.HasPrefix(trimmedLine, "<!--") && strings.HasSuffix(trimmedLine, "-->") {
-			return Uncomment(line, char)
-		}
+	if char == "<!-- -->" && strings.HasPrefix(trimmedLine, "<!--") && strings.HasSuffix(trimmedLine, "-->") {
+		return Uncomment(line, char)
+	} else if char == "<!-- -->" {
 		return Comment(line, char)
 	}
 
 	if strings.HasPrefix(trimmedLine, char) {
 		return Uncomment(line, char)
-	} else {
-		return Comment(line, char)
 	}
+	return Comment(line, char)
 }
